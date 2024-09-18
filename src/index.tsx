c0b0e69462e4ab59b5172c6011ea51916a73bd62
olga-kacala <olga.kacala@gmail.com>
2024-09-18 10:14:46 +0200

    strict mode

--- src/index.tsx
+++ src/index.tsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 import App from './App';
@@ -8,7 +9,9 @@ import '@fortawesome/fontawesome-free/css/all.min.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
+  <React.StrictMode>
   <Provider store={store}>
     <App />
   </Provider>
+</React.StrictMode>
 );
